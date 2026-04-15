2cc71b4b57c21878e2cde5c8d11646dcdcde4b04
Knifos <Knifos@hotmail.com>
2026-04-15 17:05:23 +0200

    Create Barre de recherche fixe.user.js

---
+++ Barre de recherche fixe.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Barre de recherche fixe | Knifos
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Barre de recherche fixe
+// @match        https://www.jeuxvideo.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const style = document.createElement('style');
+    style.innerHTML = `
+        .buttonsNavbar__sticky {
+            position: relative !important;
+            top: 0 !important;
+        }
+
+        .buttonsNavbar {
+            background-color: var(--header-background-color);
+        }
+    `;
+
+    document.head.appendChild(style);
+})();
